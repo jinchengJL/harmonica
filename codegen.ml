12d4bbe9b5023be234e51ff0074702c8f794963c
Jincheng Li <jl4569@columbia.edu>
2016-11-06 22:33:02 -0500

    Update module name to Harmonica

--- codegen.ml
+++ codegen.ml
@@ -19,7 +19,7 @@ module StringMap = Map.Make(String)
 
 let translate (globals, functions) =
   let context = L.global_context () in
-  let the_module = L.create_module context "MicroC"
+  let the_module = L.create_module context "Harmonica"
   and i32_t  = L.i32_type  context
   and i8_t   = L.i8_type   context
   and i1_t   = L.i1_type   context
