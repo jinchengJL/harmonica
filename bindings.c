d14109df78989dd1195058554f49a163b2be6fb4
Jincheng Li <jl4569@columbia.edu>
2016-12-17 14:47:31 -0500

    Upload bindings

---
+++ bindings.c
@@ -0,0 +1,16 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFSIZE 4096
+
+void str_concat(void *str1, void *str2)
+{
+    strcat((char *)str1, (char *)str2);
+}
+
+void int_to_string(int n, void *buf)
+{
+    sprintf(buf, "%d", n);
+}
